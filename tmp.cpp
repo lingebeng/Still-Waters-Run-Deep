3a2900cd5010e530373493732cb1e69892885b49
0xlhf <31420213@njau.edu.cn>
2023-11-18 21:42:27 +0800

    11-28 单调栈与图上dp

--- tmp.cpp
+++ tmp.cpp
@@ -1,28 +1,46 @@
 #include<iostream>
-#include<cstring>
 #define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define ll long long
-const int N = 4e4 + 1,MOD = 1e9 + 7;
+const int N = 5e3 + 5;
 using namespace std;
-int dp[N],t;
+int e[N][3];
+int dp[N][N],from[N][N],t,ans,n,m,path[N];
 void solve()
 {
-	int x;cin >> x;
-	cout << dp[x] << '\n';
+	int maxT;
+	cin >> n >> m >> maxT;
+	for(int i = 0;i < m;i++) cin >> e[i][0] >> e[i][1] >> e[i][2];
+	for(int i = 0;i < n;i++) for(int j = 1;j <= n;j++) dp[i][j] = maxT + 1;
+	dp[0][1] = 0;
+	for(int i = 1;i < n;i++)
+	{
+		for(int j = 0;j < m;j++)
+		{
+			int u = e[j][0],v = e[j][1],w = e[j][2];
+			int tmp = dp[i - 1][u] + w;
+			if(tmp < dp[i][v])
+			{
+				dp[i][v] = tmp;
+				from[i][v] = u;
+			}
+		}
+		if(dp[i][n] <= maxT) ans = i;
+	}
+	cout << ans + 1 << '\n';
+	int u = n;
+	for(int i = ans;i >= 0;i--)
+	{
+		path[i] = u;
+		u = from[i][u]; 
+	}
+	for(int i = 0;i <= ans;i++)
+	{
+		cout << path[i] << ' ';
+	}
+	cout << '\n';
 }
 signed main()
 {
 	fast
-	dp[0] = 1;
-	for(int i = 1;i <= N;i++)
-	{
-		string s = to_string(i);
-		if(string(s.rbegin(),s.rend()) == s)
-		{
-			for(int j = i;j <= N;j++)
-			dp[j] = (dp[j] + dp[j - i]) % MOD;
-		}
-	}
-	cin >> t;
+	t = 1;
 	while(t--) solve();
 }
---
+++ tmp.py
@@ -0,0 +1,5 @@
+from bisect import bisect_left
+
+a = [(1,(3,3)),(2,(2,3))]
+
+print(bisect_left(a,(1,)))
